9528f6ae9acd47da6a298a36ae9793b803e394e3
Sergey Stepanov <Deluta1989@gmail.com>
2024-05-26 18:08:33 +0300

    feat: add header component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
+import Header from "./components/header";
 
 function App() {
   return (
     <div>
-      <div className="header">
-          <div className="header-logo"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/VK_Full_Logo_%282021-present%29.svg/2560px-VK_Full_Logo_%282021-present%29.svg.png" alt=""/> </div>
-          <div className="header-find">
-              <label htmlFor=""></label>
-              <input type="text"/>
-          </div>
-          <div className="header-events"><img src="https://icon-icons.com/icons2/936/PNG/512/musical-bell-outline_icon-icons.com_73481.png" alt=""/></div>
-          <div className="header-music"><img src="https://pic.onlinewebfonts.com/thumbnails/icons_562056.svg" alt=""/></div>
-          <div className="header-services"><img src="https://icon-icons.com/icons2/2719/PNG/512/dots_nine_icon_175235.png" alt=""/></div>
-          <div className="header-profile"><img src="https://icon-icons.com/icons2/1904/PNG/512/profile_121261.png" alt=""/></div>
-      </div>
+    <Header></Header>
     </div>
   );
 }
